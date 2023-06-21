69f64be5d306a36b844168cf21ecad21ffb102bf
sharmajeet <jeetsharma31179@gmail.com>
2023-06-21 18:47:02 +0530

    updated

--- StudentDetails.java
+++ StudentDetails.java
@@ -1,32 +1,33 @@
 import java.util.*;
 
 public class StudentDetails {
+
+    public static void details(int id[], String name[], int marks[]) {
+        for (int i = 0; i < id.length; i++) {
+            System.out.println("Student Name : " + name[i]);
+            System.out.println("Student Id : " + id[i]);
+            System.out.println("Student Marks : " + marks[i]);
+
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name[] = new String[5];
-        int id[] = new id[5];
-        int marks[] = new marks[5];
-        System.out.println("Enter student name :");
+        int id[] = new int[5];
+        int marks[] = new int[5];
+
         for (int i = 0; i < id.length; i++) {
+
+            System.out.print("Enter student id :");
             id[i] = sc.nextInt();
+
+            System.out.print("Enter student marks :");
+            marks[i] = sc.nextInt();
+            sc.nextLine();
+            System.out.println();
         }
-        System.out.println("Enter student id :");
-
-        for (int j = 0; j < name.length; j++) {
-            name[j] = sc.nextLine();
-        }
-        System.out.println("Enter student marks :");
-
-        for (int k = 0; k < marks.length; k++) {
-            id[k] = sc.nextInt();
-        }
-
-    }
-
-
-    public static void detils(int id[] , String name[] , int marks[]){
-        for(int i=o)
-        
+        details(id, name, marks);
     }
 
 }
