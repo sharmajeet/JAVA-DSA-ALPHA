a1bcb009c1115f8f5976bcd3343394d0ffde990c
sharmajeet <jeetsharma31179@gmail.com>
2023-06-21 18:24:04 +0530

    Student file created

---
+++ StudentDetails.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class StudentDetails {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name[] = new String[5];
+        int id[] = new id[5];
+        int marks[] = new marks[5];
+        System.out.println("Enter student name :");
+        for (int i = 0; i < id.length; i++) {
+            id[i] = sc.nextInt();
+        }
+        System.out.println("Enter student id :");
+
+        for (int j = 0; j < name.length; j++) {
+            name[j] = sc.nextLine();
+        }
+        System.out.println("Enter student marks :");
+
+        for (int k = 0; k < marks.length; k++) {
+            id[k] = sc.nextInt();
+        }
+
+    }
+
+
+    public static void detils(int id[] , String name[] , int marks[]){
+        for(int i=o)
+        
+    }
+
+}
